9405ed1cc8560c7003c0ddcd29cb116a7fe059fa
Tigran Mkrtchyan <tigran.mkrtchyan1@outlook.com>
2024-12-17 10:41:46 +0100

    edited some CSS

--- src/Components/app-employees/app-employee-item/app-employee-item.jsx
+++ src/Components/app-employees/app-employee-item/app-employee-item.jsx
@@ -1,9 +1,12 @@
+import './app-employee-item.css';
 
+import trashIcon from '../../icons/trash.svg';
 
 export default function AppEmployeeItem () {
     return (
-        <div className="employee-item">
-            
+        <div className="employee-item repeating-block-box">
+            <h2>Adding Employee</h2>
+            <img src={trashIcon} alt="icons" />
         </div>
     )
 }
